9e77c277623e325ed3542e949df1a0afaa5bfe5e
Malice776 <marinerapin@me.com>
2025-09-30 16:24:36 +0200

    test donut

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,6 +138,7 @@ if page == '📊 Graphiques':
                 title='Répartition des visites par restaurant',
                 hole=0.3,  # donut
                 labels={'visites': 'Nombre de visites'}
+                width=600, height=500
             )
             fig3.update_traces(texttemplate='%{value}')  # affiche les valeurs exactes
             st.plotly_chart(fig3, use_container_width=True)
